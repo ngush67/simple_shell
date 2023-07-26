ad7a1cf72957552c1fe208e3eef0818bc9367ed5
Ngush <olsonngula@gmail.com>
2023-07-26 06:20:45 -0700

    Fix

--- shell.h
+++ shell.h
@@ -1,17 +1,17 @@
 #ifndef SHELL
 #define SHELL
 
-#include <dirent.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
 /**
  * struct list - linked list for environmental variables
@@ -26,39 +26,37 @@ typedef struct list
 } list_t;
 
 /* function prototypes */
-list_t *add_end_node(list_t **head, char *str);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-int built_in(char **token, list_t *env, int num, char **command);
-void cant_cd_to(char *str, int c_n, list_t *env);
-int _cd(char **str, list_t *env, int num);
-char **c_str_tok(char *str, char *delm);
-int _env(char **str, list_t *env);
-list_t *env_linked_list(char **env);
-int _execve(char *argv[], list_t *env, int num);
-int __exit(char **s, list_t *env, int num, char **command);
-int delete_nodeint_at_index(list_t **head, int index);
-void free_double_ptr(char **str);
-void free_linked_list(list_t *list);
-int find_env(list_t *env, char *str);
-char *get_env(char *str, list_t *env);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-char *ignore_space(char *str);
-void illegal_number(char *str, int c_n, list_t *env);
-char *int_to_string(int num);
-void non_interactive(list_t *env);
-void not_found(char *str, int num, list_t *env);
-size_t print_list(list_t *h);
 int prompt(char **env);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _setenv(list_t **env, char **str);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-char *_strdup(char *str);
+size_t get_line(char **str);
 int t_strlen(char *str, int pos, char delm);
+char *ignore_space(char *str);
 char **_str_tok(char *str, char *delm);
-int _unsetenv(list_t **env, char **str);
+char **c_str_tok(char *str, char *delm);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _cd(char **str, list_t *env, int num);
+int built_in(char **token, list_t *env, int num, char **command);
+void non_interactive(list_t *env);
 char *_which(char *str, list_t *env);
-
+int __exit(char **s, list_t *env, int num, char **command);
+int _execve(char *argv[], list_t *env, int num);
+void free_double_ptr(char **str);
+void free_linked_list(list_t *list);
+int _env(char **str, list_t *env);
+char *get_env(char *str, list_t *env);
+list_t *env_linked_list(char **env);
+list_t *add_end_node(list_t **head, char *str);
+size_t print_list(list_t *h);
+int delete_nodeint_at_index(list_t **head, int index);
+int _unsetenv(list_t **env, char **str);
+int _setenv(list_t **env, char **str);
+int find_env(list_t *env, char *str);
+void not_found(char *str, int num, list_t *env);
+void cant_cd_to(char *str, int c_n, list_t *env);
+void illegal_number(char *str, int c_n, list_t *env);
+char *int_to_string(int num);
 
 #endif
